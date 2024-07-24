de1a9af39f004ffb4cd1e7665e122fe4e4fc45be
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-24 20:25:56 +0300

    chore: Add form submission handling and update SVG icons

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+
+const handleSubmit = (e) => {
+    e.preventDefault(e);
+
+    const data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+};
+
+form.addEventListener("submit", handleSubmit);
