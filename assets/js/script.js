fd98379e2133655c7892344fc3562b36b33e69bc
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-26 19:47:29 +0300

    validating the email and setting the 2nd screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,30 @@
 
 const email = document.getElementById("email");
 const submit = document.getElementById("submit");
+const emailError = document.getElementById("emailError");
 const userEmail = document.getElementById("userEmail");
+const mainBody = document.getElementById("mainBody");
+const otherSect = document.getElementById("otherSect");
+
+submit.addEventListener("click", validate);
+email.addEventListener("input", check);
+
+function validate() {
+    if (email.value === "" || !email.value.includes("@")) {
+        emailError.style.display = 'block';
+        email.classList.add('error');
+        email.focus();
+    }
+    else {
+        userEmail.innerHTML = email.value;
+        email.value = "";
+        emailError.style.display = 'none';
+        email.classList.remove('error');
+        mainBody.style.display = 'none';
+        otherSect.style.display = 'flex';
+    }
+}
+function check() {
+    emailError.style.display = 'none';
+    email.classList.remove('error');
+}
